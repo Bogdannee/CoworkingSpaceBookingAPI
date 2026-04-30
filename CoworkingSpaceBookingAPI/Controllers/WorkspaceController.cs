513071a65c301d832a1e40739eaae0cfc4c43d8d
Bogdannee <bogdansavc@gmail.com>
2026-04-30 17:33:12 +0300

    added controllers templates

---
+++ CoworkingSpaceBookingAPI/Controllers/WorkspaceController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoworkingSpaceBookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkspaceController : ControllerBase
+    {
+        // GET: api/<WorkspaceController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<WorkspaceController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<WorkspaceController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<WorkspaceController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<WorkspaceController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
